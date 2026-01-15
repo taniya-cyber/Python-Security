574574678bf0efe7182d896ec89155fdbf0b70fd
taniya-cyber <taniyaarora809@gmail.com>
2026-01-15 20:39:01 +0530

    Added initialization for Empty set, list and tuple

---
+++ Basics/empty_list_tuple_set.py
@@ -0,0 +1,9 @@
+#Empty List Initialization:
+empty_list = []
+empty_list = list()
+#Empty Tuple Initialization:
+empty_tuple = ()
+empty_tuple = tuple()
+#Empty Set Initialization:
+empty_set = {} #WRONG WAY, THIS IS FOR DICTIONARY.
+empty_set = set() #Correct Way.
