4deedc095f7f3194d3418f0a63dc13b64879af1b
taniya-cyber <taniyaarora809@gmail.com>
2026-03-14 19:45:08 +0530

    Added For Loop Practice with Exmples

--- for_loop.py
+++ for_loop.py
@@ -1,6 +1,17 @@
 #Starting with basic example of For LOOP:
+#break
 clients = ['ab', 'bc' 'cd', 'ef', 'hg']
 for client in clients:
     if client == 'ef':
         break
     print(client)
+#continue
+for client in clients:
+    if client == 'ef':
+        continue
+    print(client)
+#Loop In Loop
+nums = [1,2]
+for client in clients:
+    for num in nums: 
+        print(client, num)
